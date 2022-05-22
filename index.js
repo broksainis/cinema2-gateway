54b5788d23700b5fe5f516f283ef85cef49250e1
broksainis <broksainis@gmail.com>
2022-05-23 01:15:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,11 @@ const utils = require('./utils.js');
 const moviesBackend = process.env.BACKEND_MOVIES || '0.0.0.0';
 const scheduleBackend = process.env.BACKEND_SCHEDULE || '0.0.0.0';
 
-// url to movies and schedule microservices
-// const MOVIES_API_URL = process.env.MOVIES_API_URL;
-// const SCHEDULE_API_URL = process.env.SHCEDULE_API_URL;
-
 app.use(cors());
 
 app.get("/", async (req, res) => {
     try {
-        const moviesWithSchedule = await utils.mergeMoviesWithSchedule(moviesBackend, scheduleBackend);
+        const moviesWithSchedule = await utils.mergeMoviesWithSchedule(`http://${host}:${port}${moviesBackend}`, `http://${host}:${port}${scheduleBackend}`);
         res.send(moviesWithSchedule);
     } catch (error) {
         console.error(error);

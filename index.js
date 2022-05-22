7704b96efebda29fca3d3b0e8232c8db97522f83
broksainis <broksainis@gmail.com>
2022-05-22 18:59:47 +0300

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+const cors = require('cors');
+const utils = require('./utils.js');
+
+// url to movies and schedule microservices
+const MOVIES_API_URL = process.env.MOVIES_API_URL;
+const SHCEDULE_API_URL = process.env.SHCEDULE_API_URL;
+
+app.use(cors());
+
+app.get("/", async (req, res) => {
+    try {
+        const moviesWithSchedule = await utils.mergeMoviesWithSchedule(MOVIES_API_URL, SHCEDULE_API_URL);
+        res.send(moviesWithSchedule);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.listen(port, () => {
+    console.log('Server started.');
+})
---
+++ utils.js
@@ -0,0 +1,35 @@
+const request = require('request');
+
+// get JSON data from movie and schedule api
+const getDataFromApi = async (url) => {
+    return new Promise((resolve, reject) => {
+        request(url, (error, res, body) => {
+            if (!error && res.statusCode == 200) {
+                resolve(JSON.parse(body));
+            } else {
+                reject(error);
+            }
+        });
+    })
+};
+
+// merge movies with schedule
+const mergeMoviesWithSchedule = async (moviesUrl, scheduleUrl) => {
+    const movies = await getDataFromApi(moviesUrl);
+    const schedule = await getDataFromApi(scheduleUrl);
+    movies.forEach(movie => {
+        movie.Events = [];
+        if (schedule) {
+            schedule.forEach(event => {
+                if (event.OriginalTitle === movie.OriginalTitle) {
+                    movie.Events.push(event);
+                }
+            })
+        }
+    });
+    return movies;
+};
+
+module.exports = {
+    mergeMoviesWithSchedule
+}
